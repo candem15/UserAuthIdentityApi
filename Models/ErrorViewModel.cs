e268268dd2c17c923e21aaa34ec19fd4a81ed99b
Eray <eraybrbr@gmail.com>
2021-08-22 23:47:14 +0300

    Initial commit
    Following commands runned.
    "dotnet new mvc -o UserAuthIdentityApi"
    "dotnet new mvc --auth Individual -- force"
    "git init"
    "dotnet new gitignore"

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UserAuthIdentityApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
